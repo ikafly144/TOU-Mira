32f344eee84c9d0d9ccfd36c6b86d72bc59af5e5
ikafly144 <ikafly144@sabafly.net>
2026-05-02 18:47:49 +0900

    fix(events): 死亡したプレイヤーの役職取得時のnullチェックを追加

--- TownOfUs/Events/GhostRoleEvents.cs
+++ TownOfUs/Events/GhostRoleEvents.cs
@@ -98,6 +98,8 @@ public static class GhostRoleEvents
             if (!isSkipped)
             {
                 var deadCrew = PlayerControl.AllPlayerControls.ToArray().Where(x =>
+                    x.Data != null &&
+                    x.GetRoleWhenAlive() != null &&
                     (x.Data.IsDead || x == exiled) && x.GetRoleWhenAlive().IsCrewmate() && !x.HasModifier<AllianceGameModifier>() &&
                     x.CanGetGhostRole() &&
                     x.Data.Role).ToList();
@@ -125,6 +127,8 @@ public static class GhostRoleEvents
             if (!isSkipped)
             {
                 var deadNeutral = PlayerControl.AllPlayerControls.ToArray().Where(x =>
+                    x.Data != null &&
+                    x.GetRoleWhenAlive() != null &&
                     x.Data.IsDead && x != exiled && x.GetRoleWhenAlive().IsNeutral() &&
                     !x.GetRoleWhenAlive().DidWin(GameOverReason.CrewmatesByVote) &&
                     x.CanGetGhostRole() &&
